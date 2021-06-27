f00dd16eef846cd2261eb1c71436f90766e61245
MNSHN72 <gigamilk69@gmail.com>
2021-06-27 09:35:07 +0900

    Movement Prototype 2021-06-27 0934
    # putting some things I've learned into practice such as:
    # project folder structure
    # using a sandbox project to organize asset pack imports

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] private Vector3 defaultPosition = new Vector3(0f, 0.5f, 0f);
     [SerializeField] private float moveLimit = 5f;
-    private 
     private bool shouldReset => Input.GetKey(KeyCode.Space);
     private bool shouldMoveRight => Input.GetAxis("Horizontal") > 0 && this.gameObject.transform.position.x <= moveLimit;
     private bool shouldMoveLeft => Input.GetAxis("Horizontal") < 0 && this.gameObject.transform.position.x >= -moveLimit;
@@ -15,7 +14,6 @@ public class PlayerMovement : MonoBehaviour
     private bool shouldMoveBack => Input.GetAxis("Vertical") < 0 && this.gameObject.transform.position.z >= -moveLimit;
     void Update()
     { 
-        transform.TransformDirection
         if (shouldMoveRight)
         {
             this.gameObject.transform.position += Vector3.right * Time.deltaTime * _moveSpeed;
