ab3ee8762b852ccc1d5e820412d529675c23681c
MNSHN72 <gigamilk69@gmail.com>
2021-06-28 14:01:55 +0900

    Movement Prototype 2021-06-28 14:01
    # made it so that the player model will now face the direction of its velocity
    # implimented sprint

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
-    [SerializeField] private float _jumpSpeed = 1f;
     [SerializeField] private float _sprintSpeed = 8f;
+    [SerializeField] private float _currentSpeed = 5f;
+    [SerializeField] private float _jumpSpeed = 1f;
     [SerializeField] private float _gravity = .25f;
 
     [SerializeField] private Vector3 _moveDirection = Vector3.zero;
+    private Vector3 _viewingVector;
+    private Transform _playerModel;
     private CharacterController _characterController;
-    private void Awake() => _characterController = GetComponent<CharacterController>();
+    private void Awake()
+    {
+        _playerModel = transform.GetChild(0);
+        _characterController = GetComponent<CharacterController>();
+    }
     private void Update()
     {
-        Debug.Log(_characterController.isGrounded);
+        //Debug.Log(_characterController.isGrounded);
+        Debug.Log(_characterController.velocity);
         if (_characterController.isGrounded && Input.GetButtonDown("Jump"))
         {
             PlayerJumped = true;
+        }
+        if (Input.GetButton("Sprint"))
+        {
+            _currentSpeed = _sprintSpeed;
+        }
+        else
+        {
+            _currentSpeed = _moveSpeed;
         }
     }
     private void FixedUpdate()
@@ -29,7 +45,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 inputDirection = new Vector3(horizontal, 0, vertical);
         Vector3 transformDirection = transform.TransformDirection(inputDirection);
 
-        Vector3 groundMovement = _moveSpeed * Time.deltaTime * transformDirection;
+        Vector3 groundMovement = _currentSpeed * Time.deltaTime * transformDirection;
 
         _moveDirection = new Vector3(groundMovement.x, _moveDirection.y, groundMovement.z);
 
@@ -38,14 +54,21 @@ public class PlayerMovement : MonoBehaviour
             _moveDirection.y = _jumpSpeed;
             PlayerJumped = false;
         }
-        else if (_characterController.isGrounded==false)
+        else if (_characterController.isGrounded == false)
         {
-            _moveDirection.y -= _gravity * Time.deltaTime; 
+            _moveDirection.y -= _gravity * Time.deltaTime;
+        }
+
+        _viewingVector = new Vector3(_characterController.velocity.x, 0f, _characterController.velocity.z);
+        if (_viewingVector != Vector3.zero)
+        {
+            _playerModel.transform.rotation = Quaternion.LookRotation(_viewingVector, Vector3.up); 
         }
 
         _characterController.Move(_moveDirection);
 
     }
     private bool PlayerJumped;
+
 
 }
